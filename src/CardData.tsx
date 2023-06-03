489a3e8d6cddf6dd9e11049177a5e90611e1fdf6
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-02 19:09:24 -0500

    feat: create array icon

--- src/CardData.tsx
+++ src/CardData.tsx
@@ -3,5 +3,88 @@ import React from 'react';
  
 
 export default [
-    {}
+    {
+        id:1,
+        icon:"fa-solid fa-ghost",
+        status:'down'
+    },
+    {
+        id:2,
+        icon:"fa-solid fa-ghost",
+        status:'down'
+    },
+    {
+        id:3,
+        icon:"fa-solid fa-dragon",
+        status:'down'
+    },
+    {
+        id:4,
+        icon:"fa-solid fa-dragon",
+        status:'down'
+    },
+    {
+        id:5,
+        icon:"fa-solid fa-fire",
+        status:'down'
+    },
+    {
+        id:6,
+        icon:"fa-solid fa-fire",
+        status:'down'
+    },
+    {
+        id:7,
+        icon:"fa-solid fa-house",
+        status:'down'
+    },
+    {
+        id:8,
+        icon:"fa-solid fa-house",
+        status:'down'
+    },
+    {
+        id:9,
+        icon:"fa-solid fa-snowflake",
+        status:'down'
+    },
+    {
+        id:10,
+        icon:"fa-solid fa-snowflake",
+        status:'down'
+    },
+    {
+        id:11,
+        icon:"fa-solid fa-carrot",
+        status:'down'
+    },
+    {
+        id:12,
+        icon:"fa-solid fa-carrot",
+        status:'down'
+    },
+    {
+        id:13,
+        icon:"fa-solid  fa-car",
+        status:'down'
+    },
+    {
+        id:14,
+        icon:"fa-solid  fa-car",
+        status:'down'
+    },
+    {
+        id:15,
+        icon:"fa-solid  fa-heart",
+        status:'down'
+    },
+    {
+        id:16,
+        icon:"fa-solid  fa-heart",
+        status:'down'
+    },
+
+    
+    
+    
 ]
